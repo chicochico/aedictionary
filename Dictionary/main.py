3a0a1ce9ef8b3c9b9adf63d01f4a2a0952cf52b0
Chico <Chico@Chico-PC>
2013-10-14 01:26:26 -0300

    ...

--- Dictionary/main.py
+++ Dictionary/main.py
@@ -4,7 +4,7 @@ from file import Word
 from file import TextPreProcessor
 from table import Table
 from hash_table import Hash
-from avl_tree import AVL
+from avl_tree import AVL, Node
 
     
 if __name__ == '__main__':
@@ -20,8 +20,28 @@ if __name__ == '__main__':
     
     l = [12,26,29,30,40,31,42,47,85,88,10]
     
+#     avl_tree.root = Node()
+#     avl_tree.root.word = 4
+#     avl_tree.root.rchild = Node()
+#     avl_tree.root.rchild.word = 5
+#     avl_tree.root.rchild.rchild = Node()
+#     avl_tree.root.rchild.rchild.word = 7
+#     
+#     print(avl_tree.root.word)
+#     print(avl_tree.root.rchild.word)
+#     print(avl_tree.root.rchild.rchild.word)
+#     
+#     bnode = avl_tree.root.rchild
+#     
+#     avl_tree.root = avl_tree.rr(avl_tree.root, bnode)
+#     print("fuck")
+#     
+#     print(avl_tree.root.word)
+#     print(avl_tree.root.lchild.word)
+#     print(avl_tree.root.rchild.word)
+    
     for i in l:
         avl_tree.add(i)
-
+ 
     print(avl_tree.root.word)
     print(avl_tree.root.rchild.word)
